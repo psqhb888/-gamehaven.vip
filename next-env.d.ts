3d363f740bb85ebc654b079a94a21cd800f152af
psqhb888 <132559065+psqhb888@users.noreply.github.com>
2025-03-26 13:20:24 +0800

    Add files via upload

---
+++ next-env.d.ts
@@ -0,0 +1,20 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare module '*.svg' {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+} 
---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'zh'],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: ['localhost'],
+  },
+};
+
+module.exports = nextConfig; 
